37df3c1a94b55053a0912fb972e61c20f6f6c0c2
Isaac Medeiros <isaac.medeiros.704@ufrn.edu.br>
2022-06-05 03:38:07 -0300

    primeiro commit

---
+++ src/br/ufrn/imd/controle/NaoInteiroException.java
@@ -0,0 +1,18 @@
+package br.ufrn.imd.controle;
+
+public class NaoInteiroException extends Exception{
+
+    protected int num;
+    protected int denom;
+
+    public NaoInteiroException(int num, int denom) {
+        super();
+        this.num = num;
+        this.denom = denom;
+    }
+
+    @Override
+    public String toString() {
+        return "Resultado de " + num + "/" + denom + " não é inteiro!";
+    }
+}
